344f54bb9ba7540f60274b99e8dbc3d6439c809a
pedrocustodio790 <pedrocustodio2006@hotmail.com>
2025-05-28 15:50:15 -0300

    MaterialDTO.java

--- provaback2/provaback2/src/main/java/com/example/provaback2/DTO/MaterialDTO.java
+++ provaback2/provaback2/src/main/java/com/example/provaback2/DTO/MaterialDTO.java
@@ -1,17 +1,14 @@
-package com.example.provaback2.DTO;
+package com.example.provaback2.dto;
 
 import lombok.Data;
 
 import java.math.BigDecimal;
+
 @Data
 public class MaterialDTO {
-
     private Long id;
-    private String Nome;
+    private String nome;
     private String tipo;
     private String especificacao;
     private BigDecimal quantidade;
-
-
 }
-
